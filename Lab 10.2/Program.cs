88f84395891b61dabb8a4511d6dd9539cbecc8da
ipz21 <ipz21@KYCHORT>
2023-02-11 14:46:56 +0200

    Add project files.

---
+++ Lab 10.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-5, 6);
+            }
+            Array.ForEach(arr, x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            Console.WriteLine(arr.Where(x => x > 1).ToArray().Select(x => x * x).Sum());
+
+            Console.ReadKey();
+        }
+    }
+}
